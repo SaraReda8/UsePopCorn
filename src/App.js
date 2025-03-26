591f66b04c6b3dbca29ebd36493d7b52a63b136d
SaraReda8 <sara000537@gmail.com>
2025-03-26 07:22:30 +0200

    Updated API key & improved responsiveness across all devices

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { useMovies } from "./hooks/useMovies";
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
-const KEY = "f84fc31d";
+const KEY = "27a71065";
 
 export default function App() {
   const [query, setQuery] = useState("");
--- src/hooks/useMovies.js
+++ src/hooks/useMovies.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-const KEY = "f84fc31d";
+const KEY = "27a71065";
 
 export function useMovies(query) {
   const [movies, setMovies] = useState([]);
